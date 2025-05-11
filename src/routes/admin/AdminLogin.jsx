165d4dab7e8defda4e8b16620bdf9af831b7463c
Salma Mostafa <salmaamostafa2005@gmail.com>
2025-05-11 06:21:34 +0300

    extracted login styles

--- src/routes/admin/AdminLogin.jsx
+++ src/routes/admin/AdminLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./AdminShared.css";
+import "./AdminLogin.css";
 
 const AdminLogin = () => {
   const [email, setEmail] = useState("");
